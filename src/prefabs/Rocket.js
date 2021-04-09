8afde2288c76e5f2bba50321aee9eed18bf3a50f
Victoria Rafaelian <victoria.rafaelian17@gmail.com>
2021-04-09 14:04:00 -0700

    added rocket firing controls

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,14 +3,32 @@ class Rocket extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this); //super important
         this.movementSpeed = 2;
+        this.isFiring = false;
     }
 
     update() {
-        if(keyLeft.isDown) {
-            this.x -= this.movementSpeed;
-        }
-        if(keyRight.isDown) {
-            this.x += this.movementSpeed;
+        if(this.isFiring) {
+            this.y -= this.movementSpeed;
+            if(this.y < this.borderUISize*2) {
+                this.y = game.config.height - borderUISize - borderPadding;
+                this.isFiring = false;
+            }
+        } else {
+            if(keyLeft.isDown) {
+                this.x -= this.movementSpeed; //consistent movement
+            }
+            if(keyRight.isDown) {
+                this.x += this.movementSpeed;
+            }
+
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+            } //so that you are not always firing
+
+            this.x = Phaser.Math.Clamp(
+                this.x, 
+                borderUISize + borderPadding, //lowerbound
+                game.config.width - borderUISize - borderPadding); //upperbound
         }
     }
 }
